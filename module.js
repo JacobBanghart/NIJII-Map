019fc4fe1cbcdd589adc4256bcea86f8e5f68e1f
Jacob Banghart <jacobmbanghart@gmail.com>
2024-08-17 22:24:37 -0500

    init
    add my local files in case of bad thing happening

---
+++ module.js
@@ -0,0 +1,50 @@
+let map;
+
+async function initMap() {
+  // Request needed libraries.
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+    "marker",
+  );
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(39.8097343, -98.5556199),
+    zoom: 5,
+    mapId: "TRIBES_MAP",
+  });
+
+  const iconBase =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
+  const icons = {
+    star: {
+        icon: "star-svgrepo-com.svg",
+    }
+  };
+  const locations = [
+    {
+      position: new google.maps.LatLng(43.02720939984191, -112.4333469669332),
+      type: "star",
+    },
+  ];
+
+  for (let i = 0; i < locations.length; i++) {
+    const iconImage = document.createElement("img");
+
+    iconImage.src = icons[locations[i].type].icon;
+    const marker = new google.maps.marker.AdvancedMarkerElement({
+      map,
+      position: locations[i].position,
+      // content: iconImage,
+    });
+  }
+
+  map.data.loadGeoJson('Federal_American_Indian_Reservations_v1_-6588254737828329119.geojson');
+  map.data.setStyle({
+    fillColor: '#FFCCCC',
+    strokeColor: 'red',
+    strokeWeight: 2,
+    strokeOpacity: 0.5
+  })
+}
+
+initMap();
