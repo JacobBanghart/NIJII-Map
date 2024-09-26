b5ea9eb1d0b8ed2382b01ed068404a37a3ca7aa6
JacobBanghart <jacobmbanghart@gmail.com>
2024-09-26 21:38:22 +0000

    add snowflake and fix popup issues

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ import { Feature, Map, Overlay, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { fromLonLat } from "ol/proj";
 import OSM from "ol/source/OSM";
-import { Vector } from "ol/source";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import Style from "ol/style/Style";
 import Icon from "ol/style/Icon";
-import imgUrl from "./star.svg";
-import Fill from "ol/style/Fill";
+import star from "./star.svg";
+import snowflake from "./snowflake.svg";
 
 (async () => {
+  let popupOpen = false;
   const request = await fetch(
     "https://jacobmbanghart.wixsite.com/kittens/_functions/multiply"
   );
   const results = await request.json();
+  console.log(results);
   const popup = new Overlay({
     element: document.getElementById("popup"),
     autoPan: {
@@ -38,21 +39,30 @@ import Fill from "ol/style/Fill";
           ])
         ),
         title: row.popupTitle,
+        icon: row.icon,
+        content: row.popupContent,
       })
     );
   });
+  const icons = {
+    star: new Style({
+      image: new Icon({
+        anchor: [0.53, 0.65],
+        src: star,
+      }),
+    }),
+    snowflake: new Style({
+      image: new Icon({
+        anchor: [0.53, 0.65],
+        src: snowflake,
+      }),
+    }),
+  };
   const vectorLayer = new VectorLayer({
     source: new VectorSource({
       features: [...points],
     }),
-    style: () => {
-      return new Style({
-        image: new Icon({
-          anchor: [0.53, 0.65],
-          src: imgUrl,
-        }),
-      });
-    },
+    style: (feature) => icons[feature.get("icon")],
   });
 
   const map = new Map({
@@ -80,17 +90,21 @@ import Fill from "ol/style/Fill";
     if (feature) {
       var coordinates = feature.getGeometry().getCoordinates();
       // Adjust this part depending on your data; here we're assuming the feature has a 'name' property
-      var content = `<strong>${feature.get("title")}</strong><br>`;
+      var content = `<strong>${feature.get("title")}</strong><br>${feature.get(
+        "content"
+      )}`;
 
       // Set the popup content
       document.getElementById("popup-content").innerHTML = content;
 
       // Position the popup at the feature's coordinates
       popup.setPosition(coordinates);
+      popupOpen = true;
     } else {
       // If no feature is clicked, close the popup
       popup.setPosition(undefined);
       document.getElementById("popup-closer").blur();
+      popupOpen = false;
     }
   });
   map.on("pointermove", function (event) {
@@ -98,7 +112,7 @@ import Fill from "ol/style/Fill";
       return feature;
     });
 
-    if (feature) {
+    if (feature && !popupOpen) {
       // Change the cursor to a pointer when hovering over a feature
       map.getTargetElement().style.cursor = "pointer";
     } else {
@@ -110,6 +124,7 @@ import Fill from "ol/style/Fill";
   var closer = document.getElementById("popup-closer");
   closer.onclick = function () {
     popup.setPosition(undefined);
+    popupOpen = false;
     closer.blur();
     return false;
   };
